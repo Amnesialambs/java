a43557c683ce633aabf10e1077d1cb1e44279ff4
yzy <948201156@qq.com>
2018-09-14 07:19:30 +0800

    '断续上传'

--- tools-io/io-breakPointUpload/src/main/java/com/yzy/Application.java
+++ tools-io/io-breakPointUpload/src/main/java/com/yzy/Application.java
@@ -2,11 +2,19 @@ package com.yzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.yzy.action.SocketServerListener;
 
 @SpringBootApplication
 public class Application {
 
 	
+ 	@Bean
+    public ServletRegistrationBean<SocketServerListener> MyServlet1(){
+        return new ServletRegistrationBean<SocketServerListener>(new SocketServerListener(),"/myserv/*");
+    }	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
