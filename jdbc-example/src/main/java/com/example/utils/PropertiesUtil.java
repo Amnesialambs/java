8877ac5ec427a3cb5e8543b3c24f1e25871d1c4a
yangzeyu <948201156@qq.com>
2018-09-18 18:06:09 +0800

    '123'

--- jdbc-example/src/main/java/com/example/utils/PropertiesUtil.java
+++ jdbc-example/src/main/java/com/example/utils/PropertiesUtil.java
@@ -1,5 +1,44 @@
 package com.example.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PropertiesUtil {
 
+	static Properties properties = new Properties();//资源文件类
+	
+	
+	
+	/**
+	 * 
+	* @Title: loadProperties  
+	* @Description: 通过类加载器加载配置文件
+	* 
+	*  
+	* @param @param path    参数  
+	* @return void    返回类型  
+	* @throws
+	 */
+	public void loadProperties(String path) {
+		
+		try {
+			
+			InputStream in = PropertiesUtil.class.getClassLoader().getResourceAsStream("db.properties");
+			properties.load(in);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		InputStream in = PropertiesUtil.class.getClassLoader().getResourceAsStream("db.properties");
+		properties.load(in);
+		
+		System.out.println(properties.getProperty("username"));
+	} 
 }
